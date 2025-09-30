7ad4ea2bf172b5c0f55082cd777994d2cd131f09
Syed Adeeb <abidi9897401278@gmail.com>
2025-09-30 21:54:06 +0530

    feat: remove BackgroundImage component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
 			<Banner />
 			<HighlightsSection />
 			<FeaturedDestinations />
-			<BackgroundImage />
 			<CultureHeritage />
 			<FestivalEvents />
 			<EcoWildlife />
